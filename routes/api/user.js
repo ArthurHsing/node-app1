37ab212c74789f424e1fdd5d4f4eb722f2de9d26
幸成 <573105494@qq.com>
2020-10-24 21:57:30 +0800

    The first router is added

---
+++ routes/api/user.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/test', (req, res) => {
+  res.json({ msg: "login works" });
+});
+module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI; //mongodb的uri
+const users = require('./routes/api/user');
 const app = express();
+console.dir(app);
 const port = process.env.PORT || 5000;
 /**
- * 测试
+ * 测试接口
  */
-app.get('/', function (req, res) {
-  res.send('Hello, world!');
-});
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }).then(res => {
   console.log('Mongodb connected!');
 }).catch(err => {
   console.log(err);
-})
+});
+/**
+ * 为users这个文件导出的路由配置，/api/users这个路径
+ */
+app.use('/api/users', users);
+app.get('/', function (req, res) {
+  res.send('Hello, world!');
+});
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
 });
