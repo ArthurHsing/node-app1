fa13671f5405c141f439b112e7e4b203128bbcd8
幸成 <573105494@qq.com>
2020-10-24 23:05:48 +0800

    使用body-parser来解析请求数据，使用model来对数据库的数据进行查询

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const router = express.Router();
-
+const User = require('../../models/User');
 router.get('/test', (req, res) => {
   res.json({ msg: "login works" });
+});
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (user) {
+      return res.status(200).json({ code: 0, message: '邮箱已被注册' });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+    }
+  });
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const db = require('./config/keys').mongoURI; //mongodb的uri
 const users = require('./routes/api/user');
 const app = express();
-console.dir(app);
 const port = process.env.PORT || 5000;
 /**
  * 测试接口
@@ -16,6 +16,8 @@ mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }).then(r
 /**
  * 为users这个文件导出的路由配置，/api/users这个路径
  */
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use('/api/users', users);
 app.get('/', function (req, res) {
   res.send('Hello, world!');
