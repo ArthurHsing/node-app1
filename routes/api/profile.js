e8b9d951765d6ac4c13469ff8968d2337c2aeae9
幸成 <573105494@qq.com>
2020-11-17 15:56:12 +0800

    before frontEnd

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const Profile = require('../../models/Profile');
-const { route } = require('./user');
 
 router.get('/test', (req, res) => {
   res.json({ msg: "profile works" });
